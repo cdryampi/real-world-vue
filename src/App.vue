05bfc393b808729d069883a7a7d53893afc934f9
cdryampi <cdryampi@gmail.com>
2024-12-25 05:09:50 +0100

    test github-pages

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <h1>View JS</h1>
+  <br />
+  <CarComponent
+    :brand="brand"
+    :model="model"
+    :colors="color"
+    :price="price"
+    :power="power"
+    :message="message"
+    :upPower="upPower"
+    :getPower="getPower"
+    @downPower="downPower"
+  />
+  <br />
+  <CarOptions />
+</template>
+
+<script>
+import CarComponent from './components/CarComponent.vue'
+import CarOptions from './components/CarOptions.vue'
+import { ref } from 'vue'
+
+export default {
+  components: {
+    CarComponent,
+    CarOptions,
+  },
+  setup() {
+    let brand = 'Ford'
+    let model = 'Mustang'
+    let color = ['red', 'blue', 'green']
+    let price = 27000
+    let powers = [100, 200, 300, 400, 500]
+    let power = ref(powers[0])
+    let message = {
+      title: 'Vue 3',
+      body: 'This is a test message',
+    }
+    const upPower = () => {
+      const index = powers.indexOf(power.value)
+      if (index < powers.length - 1) {
+        power.value = powers[index + 1]
+      }
+    }
+    const downPower = () => {
+      const index = powers.indexOf(power.value)
+      if (index > 0) {
+        power.value = powers[index - 1]
+      }
+    }
+
+    return {
+      brand,
+      model,
+      color,
+      price,
+      powers,
+      message,
+      power,
+      upPower,
+      downPower,
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/CarOptions.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>Car Options</h1>
+    <p>Brand: {{ brand }}</p>
+    <p>Model: {{ model }}</p>
+    <p>Color: {{ color }}</p>
+    <p>Price: {{ price }}</p>
+    <div>
+      <p>Power: {{ power }}</p>
+      <button @click="encrase">+</button>
+      <button @click="decrease">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  data() {
+    const POWERS = [80, 100, 120, 140, 160]
+    let power = ref(POWERS[0])
+
+    const encrase = () => {
+      let index = POWERS.indexOf(power.value)
+      if (POWERS[index] < POWERS[POWERS.length - 1]) {
+        power.value = POWERS[index + 1]
+      } else {
+        power.value = POWERS[0]
+      }
+    }
+    const decrease = () => {
+      let index = POWERS.indexOf(power.value)
+      if (POWERS[index] > POWERS[0]) {
+        power.value = POWERS[index - 1]
+      } else {
+        power.value = POWERS[POWERS.length - 1]
+      }
+    }
+    return {
+      brand: 'Seat',
+      model: 'Ibiza',
+      color: ['red', 'blue', 'green'],
+      price: 15000,
+      power,
+      encrase,
+      decrease,
+    }
+  },
+}
+</script>
+
+<style></style>
